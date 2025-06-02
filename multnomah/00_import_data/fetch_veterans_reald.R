7e39c5609986ad4d2c8629deffb0ff5cbc573b82
swnordstrom <swnordstrom@email.wm.edu>
2025-06-02 08:02:28 -0700

    add req for single extracts with vets + all housing costs/arrangements

--- multnomah/00_import_data/fetch_veterans_reald.R
+++ multnomah/00_import_data/fetch_veterans_reald.R
@@ -56,6 +56,39 @@ acs_extract %>%
   download_extract(download_dir = 'multnomah/01_raw_data/')
 # Downloaded as extract # 56
 
+# Download
+acs_extract %>%
+  submit_extract() %>%
+  wait_for_extract() %>%
+  download_extract(download_dir = 'multnomah/01_raw_data/')
+# Downloaded as extract # 54
+
+### Combining housing data and veterans data into one PUMS
+
+acs_extract = define_extract_micro(
+  collection = 'usa',
+  description = 'PUMS for age+disability+ancestry, Mult Co. draw 4 with housing cost included',
+  samples = 'us2023c',
+  variables = list(
+    var_spec('STATEFIP', case_selections = '41'),
+    var_spec('COUNTYICP', case_selections = '0510'),
+    'PUMA', 'AGE', 'SEX',
+    'RACAMIND', 'RACASIAN', 'RACBLK', 'RACPACIS', 'RACWHT', 'RACOTHER', 'HISPAN',
+    'ANCESTR1', 'ANCESTR2',
+    'VETSTAT', 'OCC', 'GQ',
+    'DIFFCARE', 'DIFFMOB', 'DIFFPHYS', 'DIFFREM', 'DIFFSENS',
+    'MARST', 'RELATE', 
+    # Housing cost variables:
+    'OWNERSHP', 'OWNCOST', 'RENTGRS', 'HHINCOME'
+  )
+)
+
+acs_extract %>%
+  submit_extract() %>%
+  wait_for_extract() %>%
+  download_extract(download_dir = 'multnomah/01_raw_data/')
+# Downloaded as extract # 59
+
 # # # ------------ Tabular controls
 
 tab_extract = define_extract_nhgis(
@@ -108,3 +141,29 @@ tab_extract %>%
   wait_for_extract() %>%
   download_extract(download_dir = 'multnomah/01_raw_data/')
 # Downloaded as extract # 36
+
+
+tab_extract = define_extract_nhgis(
+  description = 'tract-level veteterans+related for controls, draw 3 with housting cost tabs',
+  datasets = map2(
+    .x = list('2019_2023_ACS5a', '2019_2023_ACS5b'),
+    .y = list(
+      c(
+        'B01001', paste0('B020', c(paste0('0', 8:9), 10:13)), 'B03003', 'B21001', 
+        'B11001', 'B12001', 'B25008', 'B25093', 'B25070'
+      ),
+      c('B18101', 'B26001', 'B10063')
+    ),
+    ~ ds_spec(
+      name = .x,
+      data_tables = .y,
+      geog_levels = 'tract'
+    )
+  )
+)
+
+tab_extract %>%
+  submit_extract() %>%
+  wait_for_extract() %>%
+  download_extract(download_dir = 'multnomah/01_raw_data/')
+# Downloaded as extract # 39
